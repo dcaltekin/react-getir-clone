856e159c5dcc27a38bf8162455f3efaf64e7b85b
Dogukan Caltekin <dcaltekin@gmail.com>
2022-07-29 03:02:06 +0300

    categories-added

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,7 +1,36 @@
-import React from "react";
-
+import { useEffect, useState } from "react";
+import data from "../api/categories.json";
 function Categories() {
-  return <div>Categories</div>;
+  const [categories, setCategories] = useState();
+
+  useEffect(() => {
+    setCategories(data);
+  }, []);
+  return (
+    <div className="bg-white py-4">
+      <div className="container mx-auto w-9/12 mt-8">
+        <h2 className="text-sm font-semibold mb-4">Kategoriler</h2>
+        <div className="grid grid-cols-10">
+          {!categories.length && "Loading.."}
+          {categories &&
+            categories.map((category, index) => (
+              <div key={index}>
+                <div className="flex flex-col items-center group gap-y-2 text-center p-4 transition hover:bg-gray-100 cursor-pointer">
+                  <img
+                    className="w-12 h-12 rounded border border-gray-200"
+                    src={category.image}
+                    alt={category.title}
+                  />
+                  <span className="text-sm whitespace-nowrap text-gray-700 group-hover:text-primary-color tracking-tight">
+                    {category.title}
+                  </span>
+                </div>
+              </div>
+            ))}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Categories;
